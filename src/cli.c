530bc899e6fcfe1e9504cf3478cb0de807f99534
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-12 08:04:42 -0400

    finished tokeniser

--- src/cli.c
+++ src/cli.c
@@ -9,24 +9,25 @@ int main(int argc, char* argv[])
     int count = 0; 
     while(argv[++count] != NULL);
 
-    if(argc == 1)
+    if(argc == 1 || argc == 3)
     {
-        printf("Unsupplied arguments. \nPlease specify a string to crunch\nFormat: cruncher {STRING}\n");
+        printf("Unsupplied/Too many arguments. \nPlease specify a string to crunch\nFormat: cruncher {STRING}\n");
     }
-    else{
-        char ParsedData[]={};
-        int i;
-        for(i=0; i<count; i++)
+    else if(argc == 2){
+        char buf[] = argv[2];
+        int i = 0;
+        char *p = strtok(buf,"");
+        char *array[strlen(buf)];
+
+        while (p != NULL)
         {
-              char *Data = strtok(argv[i], ",");
-            printf("%s\n", Data);
-            while (Data != NULL)
-            {
-                /
-            }
-            
-          
+            array[i++] = p;
+            p = strtok(NULL, "");
         }
 
+        for(i=0; i<strlen(buf); i++)
+                printf("%s\n", array[i]);
+
+        
     }
 }  
