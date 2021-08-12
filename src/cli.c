22c28eab432b5a883171f5a848b07fea87fcfabf
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-11 21:43:52 -0400

    Removed `headers`

--- src/cli.c
+++ src/cli.c
@@ -1,12 +1,16 @@
-#include "include/cruncher.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[])
 {
 
     int count = 0; 
+
+    char ParsedData[] = "";
     while(argv[++count+1] != NULL);
 
-    
+    parseData(argv[count]);
 
     printf("Crunching %i argumnets \n", count);
     return 0;
