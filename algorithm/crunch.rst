14cf6a971bc528961a8f9dd13db9886416e0918e
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-11 22:07:25 -0400

    .

---
+++ algorithm/crunch.rst
@@ -0,0 +1,18 @@
+H E L L O
+↳ → → → ⮥
+
+E L L O H
+↳ → → → ⮥
+
+L L O H E
+↳ → → → ⮥
+
+L O H E L
+↳ → → → ⮥
+
+O H E L L
+↳ → → → ⮥
+
+H E L L O
+
+-- DONE --
--- src/cli.c
+++ src/cli.c
@@ -18,7 +18,7 @@ int main(int argc, char* argv[])
         int i;
         for(i=0; i<count; i++)
         {
-            printf("Argument #%i", i);
+            
         }
 
     }
