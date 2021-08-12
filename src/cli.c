a8959ed3ade5c0368d160963e34ebbd139c78b70
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-11 21:58:27 -0400

    removed placeholder count

--- src/cli.c
+++ src/cli.c
@@ -7,20 +7,19 @@ int main(int argc, char* argv[])
 
     // GET ARGUMENT COUNT
     int count = 0; 
-    while(argv[++count+1] != NULL);
+    while(argv[++count] != NULL);
 
-    if(argc >= 0)
+    if(argc == 1)
     {
         printf("Unsupplied arguments. \nPlease specify a string to crunch\nFormat: cruncher {STRING}\n");
-        
     }
     else{
 
-    int i;
-    for(i=0; i>count; i++)
-    {
-        printf("Argument #%i", i);
-    }
+        int i;
+        for(i=0; i<count; i++)
+        {
+            printf("Argument #%i", i);
+        }
 
     }
 }  
